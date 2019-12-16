72d0b4e3d7e29d1488d7de62fb0d3a5f44c5a413
jasonwongys <jasonwongys@hotmail.com>
2019-12-16 18:47:12 +0800

    remove login components

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,6 @@ const PORT = 4000;
 const bugRoutes = express.Router();
 let Bug = require('./bug.model');
 
-//
-const UserSession = require('./userSession');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -27,6 +25,17 @@ bugRoutes.route('/').get(function(req, res) {
             console.log(err);
     } else {
         res.json(bugs);
+    }
+    });
+});
+
+
+bugRoutes.route('/users/').get(function(req, res) {
+    User.find(function(err, user) {
+        if (err) {
+            console.log(err);
+    } else {
+        res.json(user);
     }
     });
 });
@@ -85,131 +94,7 @@ bugRoutes.route('/:id').delete((req, res) => {
 
 /// Login Auth Starts Here ///
 
-bugRoutes.post('/api/account/signin', (req, res, next) => {
-    const { body } = req;
-    const {
-      password
-    } = body;
-    let {
-      email
-    } = body;
-    if (!email) {
-      return res.send({
-        success: false,
-        message: 'Error: Email cannot be blank.'
-      });
-    }
-    if (!password) {
-      return res.send({
-        success: false,
-        message: 'Error: Password cannot be blank.'
-      });
-    }
-    email = email.toLowerCase();
-    email = email.trim();
-    User.find({
-      email: email
-    }, (err, users) => {
-      if (err) {
-        console.log('err 2:', err);
-        return res.send({
-          success: false,
-          message: 'Error: server error'
-        });
-      }
-      if (users.length != 1) {
-        return res.send({
-          success: false,
-          message: 'Error: Invalid'
-        });
-      }
-      const user = users[0];
-      if (!user.validPassword(password)) {
-        return res.send({
-          success: false,
-          message: 'Error: Invalid'
-        });
-      }
-      // Otherwise correct user
-    const userSession = new UserSession();
-    userSession.userId = user._id;
-    userSession.save((err, doc) => {
-        if (err) {
-            console.log(err);
-            return res.send({
-            success: false,
-            message: 'Error: server error'
-            });
-        }
-        return res.send({
-            success: true,
-            message: 'Valid sign in',
-            token: doc._id
-        });
-        });
-    });
-});
-
-bugRoutes.get('/api/account/logout', (req, res, next) => {
-    // Get the token
-    const { query } = req;
-    const { token } = query;
-    // ?token=test
-    // Verify the token is one of a kind and it's not deleted.
-    UserSession.findOneAndUpdate({
-        _id: token,
-        isDeleted: false
-    }, {
-        $set: {
-            isDeleted:true
-        }
-    }, null, (err, sessions) => {
-        if (err) {
-            console.log(err);
-            return res.send({
-            success: false,
-            message: 'Error: Server error'
-        });
-    }
-        return res.send({
-            success: true,
-            message: 'Good'
-        });
-    });
-});
-
-
-app.get('/api/account/verify', (req, res, next) => {
-    // Get the token
-    const { query } = req;
-    const { token } = query;
-    // ?token=test
-    // Verify the token is one of a kind and it's not deleted.
-        UserSession.find({
-        _id: token,
-        isDeleted: false
-        }, (err, sessions) => {
-        if (err) {
-            console.log(err);
-            return res.send({
-            success: false,
-            message: 'Error: Server error'
-            });
-        }
-        if (sessions.length != 1) {
-            return res.send({
-            success: false,
-            message: 'Error: Invalid'
-            });
-        } else {
-            // DO ACTION
-            return res.send({
-            success: true,
-            message: 'Good'
-            });
-        }
-        });
-    });
+//Add a user
 
 app.use('/bugs', bugRoutes);
 
--- backend/user.model.js
+++ backend/user.model.js
@@ -3,24 +3,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let userSchema = new Schema({
-    username: {
+    email: {
         type: String,
-        required: true,
+        require: true,
         unique: true,
-        trim: true,
-        minlength: 3
+        trim: true
     },
     password: {
         type: String,
-        required: true,
+        require: true,
+        unique: true,
         trim: true,
-        minlength: 7
     },
-    email: {
-        type: String,
-        required: true,
-        trime: true
-    },
+    
     isDelete: {
         type: Boolean,
         default: false
@@ -29,13 +24,6 @@ let userSchema = new Schema({
     timestamps: true,
 });
 
-userSchema.methids.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-}
-
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
-}
 
 const User = mongoose.model('User', userSchema);
 
