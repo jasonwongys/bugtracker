251a55702c6dc7e836f19b7af37f8b51adf0a85b
jasonwongys <jasonwongys@hotmail.com>
2020-03-23 18:38:05 +0800

    update folder structure

---
+++ client/build/precache-manifest.4cac4e72a31e06d13b01e72198506185.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6d65460409a7a370e702b0b793304b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "683e807d3f4b154293ba",
+    "url": "/static/css/2.23be29d4.chunk.css"
+  },
+  {
+    "revision": "e45a1938211eb6529c67",
+    "url": "/static/css/main.0db72c86.chunk.css"
+  },
+  {
+    "revision": "683e807d3f4b154293ba",
+    "url": "/static/js/2.bd6ac7dd.chunk.js"
+  },
+  {
+    "revision": "ff8213b3c90e45aa3cebf64b6bf86219",
+    "url": "/static/js/2.bd6ac7dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e45a1938211eb6529c67",
+    "url": "/static/js/main.6c66b0fc.chunk.js"
+  },
+  {
+    "revision": "e04d9faac0a0ef0b2253",
+    "url": "/static/js/runtime-main.e04dd3fe.js"
+  },
+  {
+    "revision": "fa065ac0cbd25a3788176d58206abe9a",
+    "url": "/static/media/loginBG.fa065ac0.jpg"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 5000;
+const passport = require('passport');
+const path = require("path");
+
+const secret = process.env.SECRET || "jasonwongysLocal"
+require("dotenv").config()
+
+
+
+mongoose.Promise = global.Promise;
+// const bugRoutes = express.Router();
+// let Bug = require('./bug.model');
+
+app.use(express.json());
+app.use(cors());
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/bugs',{ useNewUrlParser: true})
+const connection = mongoose.connection;
+
+//const db = require("./config/keys").mongoURI;
+
+// mongoose
+//     .connect(
+//         db,
+//         { useNewUrlParser: true, useUnifiedTopology: true }
+//     )
+//     .then(() => console.log("MongoDB successfully connected"))
+//     .catch( err => console.log(err));
+
+
+connection.once('open',function() {
+    console.log("Mongo DB connected successfully");
+})
+
+const bugRouter = require('./routes/bug.routes');
+const users = require("./routes/api/users");
+const projectRouter = require('./routes/project.routes');
+
+app.use(passport.initialize());
+
+
+require("./config/passport")(passport);
+
+app.use('/projects',projectRouter);
+app.use('/api/users/', users);
+app.use('/bugs', bugRouter);
+
+
+//const port = process.env.PORT || 5000
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+
+app.listen(PORT, function() {
+    console.log("Server is running on PORT: " + PORT);
+});
