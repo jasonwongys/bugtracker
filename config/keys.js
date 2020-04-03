3a6410bbbac7affaa9c094cda954785351b8e559
jasonwongys <jasonwongys@hotmail.com>
2020-04-03 20:38:11 +0800

    add connection to atlas

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://127.0.0.1:27017/bugs/?compressors=disabled&gssapiServiceName=mongodb",
+    // mongoURI: "mongodb://127.0.0.1:27017/bugs/?compressors=disabled&gssapiServiceName=mongodb",
     secretOrKey: "secret"
 };
